2b9b1c668166e00912bf41d78f939a277f25a4be
FlorianLeprevost <55445206+FlorianLeprevost@users.noreply.github.com>
2020-05-29 10:20:38 +0200

    was working yerterday bu not today: why?

--- final_reprod_v7.py
+++ final_reprod_v7.py
@@ -172,55 +172,36 @@ def simul_weight2(time_ms, time_min, time_max, funct):
     alpha=1
     P = np.identity(NGG)/alpha
     ts = funct(time_ms)
-    #error
-    e_m_array=list()
-    e_m_array.append(0)    
-    e_p_array=list()
-    e_p_array.append(0)
-    wup_array=list()
-    wup_array.append(0)    
-
-    
-    
     JGg, JGz, w, indices_zero_w = generate()
     #initializing the network activity radomly
-    wn_array=list()
-    wn_array.append(np.sqrt(np.dot(w.T,w)))
+    #w = np.zeros(NGG)[np.newaxis].T  
     x=.5*np.random.normal(0, 1, NGG)[np.newaxis].T #make it vertical
     r=np.tanh(x)
-    plt.hist(r)
-    
+    readout= np.dot(w.T,r)   
     time=np.linspace(0,time_ms,time_ms) #10 000dt = 1000ms = 1s
     
-    #stock for plot
-    x_time_array = list()
-    x_time_array.append(x[:10])
-    
-    readout= np.dot(w.T,r)
     readout_array = list()
-    readout_array.append(readout)
-    
+    x_time_array = list()
     r_array = list()
-    r_array.append(r[:10])
-    
-    dx = (-x + np.dot(gGG*JGg, r) + gGz*JGz*readout)/tau
     dx_array = list()
-    dx_array.append(dx[:10])
-    
+    #error
+    e_m_array=list()
+    e_p_array=list()
+    wup_array=list()
+    wn_array=list()
+
     #loop
-    for t in range(len(time)-1):
+    for t in range(len(time)):
         dx = (-x + np.dot(gGG*JGg, r) +gGz*JGz*readout)/tau
         x= x + dx*delta_t
-        x_time_array.append(x[:10])
-    
         r=np.tanh(x)
-        r_array.append(r[:10])
         readout= np.dot(w.T,r)
-        readout_array.append(readout)
         
+        x_time_array.append(x[:10])
+        r_array.append(r[:10])
+        readout_array.append(readout)  
         dx_array.append(dx[:10])
-    
-    
+
         if t%100 ==0:
             print(int(t), 'ms')
         
@@ -791,3 +772,199 @@ cumul=list()
 for i in range(n_comp):
     cumul.append(sum(pca_full.explained_variance_ratio_[:i])*100)
 plt.plot(cumul)
+
+#%% 6) weights_for g test
+def simul_weight_g(time_ms, time_min, time_max, funct, gGG):
+    #param for updating
+    delta_up=10
+    alpha=1
+    P = np.identity(NGG)/alpha
+    ts = funct(time_ms)
+    #error
+    e_m_array=list()
+    e_p_array=list()
+    wup_array=list()
+
+    
+    
+    JGg, JGz, w, indices_zero_w = generate()
+    w = np.zeros(NGG)[np.newaxis].T
+    #initializing the network activity radomly
+    wn_array=list()
+    x=.5*np.random.normal(0, 1, NGG)[np.newaxis].T #make it vertical
+    r=np.tanh(x)
+    plt.hist(r)
+    
+    time=np.linspace(0,time_ms,time_ms) #10 000dt = 1000ms = 1s
+    
+    #stock for plot
+    x_time_array = list()
+    
+    readout= np.dot(w.T,r)
+    readout_array = list()
+    
+    r_array = list()
+    
+    dx = (-x + np.dot(gGG*JGg, r) + gGz*JGz*readout)/tau
+    dx_array = list()
+    
+    #creating sliding window mean error
+    sl_RMS = 10
+    t=0
+    #loop
+    while sl_RMS >.01 and t<time_max:
+        t+=1
+        dx = (-x + np.dot(gGG*JGg, r) +gGz*JGz*readout)/tau
+        #dx_array.append(dx[:10])
+        
+        x= x + dx*delta_t
+        #x_time_array.append(x[:10])
+    
+        r=np.tanh(x)
+        r_array.append(r[:10])
+        readout= np.dot(w.T,r)
+        readout_array.append(readout)
+        
+        
+    
+    
+        if t%100 ==0:
+            print(int(t), 'ms', sl_RMS)
+        
+        #update weights between t_min and t_max
+        if t%delta_up ==0:
+            if t<time_min:
+                e_m=readout - ts[t]
+                wup=0
+                #e_p=0
+
+            elif t>time_max:
+                e_m = readout - ts[t]
+                wup=0
+                #e_p=0
+            else:            
+
+                P = P - np.dot(np.dot(P,r), np.dot(r.T,P))/(1+float(np.dot(np.dot(r.T,P),r)))
+                #e_p = np.dot(w.T,r) - ts[t]
+                e_m = readout - ts[t]
+                w = w - e_m * np.dot(P,r)
+                wup=np.linalg.norm(e_m * np.dot(P,r))
+                #w[indices_zero_w[:]]= 0
+                sl_RMS = np.mean(np.abs(e_m_array[-20:]))
+                #print(sl_RMS)
+                
+            #e_p_array.append(float(e_p))
+            e_m_array.append(float(e_m))
+            wup_array.append(float(wup))
+            wn_array.append(float(np.sqrt(np.dot(w.T,w))))
+            
+    e_m_RMS_array=list()
+    for t2 in (np.array(range(1000))+t):
+        dx = (-x + np.dot(gGG*JGg, r) +gGz*JGz*readout)/tau
+        #dx_array.append(dx[:10])
+
+        x= x + dx*delta_t
+        #x_time_array.append(x[:10])
+    
+        r=np.tanh(x)
+        r_array.append(r[:10])
+        
+        readout= np.dot(w.T,r)
+        readout_array.append(readout)
+        e_m=readout - ts[t2]
+        e_m_RMS_array.append(float(e_m))
+
+        
+        if t2%delta_up ==0: 
+            e_m=readout - ts[t2]
+            wup=0
+            e_m_array.append(float(e_m))
+            wup_array.append(float(wup))
+            wn_array.append(float(np.sqrt(np.dot(w.T,w))))
+
+
+    #plot
+#    offset
+    offset=np.array([(np.linspace(0,18,10)) for i in range(t+1000)])
+    # plt.subplot(4,1,1)
+    # mat_activity = np.squeeze(np.asarray(x_time_array))
+    # plt.plot(time[:], mat_activity+offset)
+    # plt.ylabel('membrane potential x')
+    # plt.xlim([0,time_ms])
+    # plt.axvline(time_min,c="black")
+    # plt.axvline(time_max,c="black")
+    plt.subplot2grid((4,1),(2,0),1,1)
+    plt.plot(np.squeeze(readout_array[:]))
+    plt.plot(ts[:])
+    plt.ylabel('Readout')
+    plt.xlim([0,t2])
+    plt.ylim([-2,2])
+    plt.xticks([])
+    plt.axvline(time_min,c="black")
+    plt.axvline(t,c="black")
+
+    plt.subplot2grid((4,1),(0,0),2,1)
+    mat_activity_r = np.squeeze(np.array(r_array))
+    plt.plot(mat_activity_r+offset)
+    plt.ylabel('Firing rate (Hz)')
+    plt.xlim([0,t2])
+    plt.xticks([])
+    plt.axvline(time_min,c="black")
+    plt.axvline(t,c="black")
+    
+    plt.subplot2grid((4,1),(3,0),1,1)
+    plt.plot(np.linspace(0,t+1000,len(e_m_array)), e_m_array, label='error')
+    plt.plot(np.linspace(0,t+1000,len(e_m_array)), np.array(wup_array)*10, label='update ')
+    #plt.plot(np.linspace(0,time_ms,len(e_m_array)), np.array(e_p_array)*10, label='error_p')
+    plt.plot(np.linspace(0,t+1000,len(e_m_array)), np.array(wn_array), label='w norm')
+    plt.xlabel('Time (ms)')
+    plt.ylabel('error')
+    plt.xlim([0,t2])
+    plt.axvline(time_min,c="black")
+    plt.axvline(t,c="black")
+    plt.legend(loc='best')    
+    
+    RMS = np.sqrt(sum(np.array(e_m_RMS_array)*np.array(e_m_RMS_array)))
+    wn=np.sqrt(np.dot(w.T,w))
+    last_error=sl_RMS
+    return wn, RMS, t, last_error
+
+#%%run many simulations with different gs
+#condition to consider that learned = mean absolute error measured 
+# only at the time of updating = when probably highest
+# so this mean of the last 20 updates <0.01
+wn_list=list()
+RMS_list=list()
+t_list = list()
+last_error_array=list()
+
+for gGG in [.8,.9,1,1.1,1.2,1.3,1.4,1.5,1.56,1.6]:
+    for k in range(10):
+        try:
+            wn, RMS,t, l_e =simul_weight_g(41400,400,40400, f_per_pap, gGG)
+        except:
+            wn, RMS,t = 'Na','Na','Na'
+        wn_list.append(wn)
+        RMS_list.append(RMS)
+        t_list.append(t)
+        last_error_array.append(l_e)
+
+#%%
+import pandas as pd
+gGG_l=np.repeat([.8,.9,1,1.1,1.2,1.3,1.4,1.5,1.56,1.6], 10)
+l_time = (np.array(t_list)-400)/400
+wn_list=np.squeeze(np.array(wn_list))
+
+dtf = {'w':wn_list, 'RMS error':RMS_list, '# periods trained':l_time, 'last error':last_error_array, 'gGG':gGG_l }
+dtf = pd.DataFrame(dtf)
+dtf.to_csv('gGG_sim.csv',index=False)    
+dtf_less=dtf.loc[10:]
+dtf_less.boxplot(column='# periods trained',by='gGG')
+dtf_less.boxplot(column='RMS error',by='gGG')
+dtf_less.boxplot(column='w',by='gGG')
+
+plt.figure()
+dtf.plot.box()
+plt.title('test')
+bp = dtf_less.boxplot(by='gGG')
+bp.title('test')
